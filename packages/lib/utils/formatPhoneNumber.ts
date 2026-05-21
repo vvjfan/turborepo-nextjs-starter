d48233b91af67dd52d7111228fb95db52a919388
Dev.Fan <dev.0xffan@outlook.com>
2026-05-21 11:10:55 +0800

    feat: add formatPhoneNumber utility

---
+++ packages/lib/utils/formatPhoneNumber.ts
@@ -0,0 +1,8 @@
+import { parsePhoneNumberWithError } from "libphonenumber-js/max";
+
+export const formatPhoneNumber = (phoneNumber: string) => {
+  const parsedPhoneNumber = parsePhoneNumberWithError(phoneNumber);
+  return parsedPhoneNumber?.isValid()
+    ? parsedPhoneNumber.formatInternational()
+    : phoneNumber;
+};
